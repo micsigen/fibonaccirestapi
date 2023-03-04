370892d6736226eb1bdbe2ce316dfed984d9701e
major1balaz525 <major.balazs@telekom.hu>
2023-03-04 22:18:49 +0100

    feat: add needed files

---
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/FibonaccirestapiApplication.kt
@@ -0,0 +1,11 @@
+package hu.obuda.devops.fibonaccirestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FibonaccirestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FibonaccirestapiApplication>(*args)
+}
---
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/FibonaccirestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.obuda.devops.fibonaccirestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FibonaccirestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
