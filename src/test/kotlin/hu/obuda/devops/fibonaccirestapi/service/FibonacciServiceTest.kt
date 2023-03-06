e6c851d153631215188e632ab1faf9de492e2e60
Oszvald Gergő <oszvaldgergo@hotmail.com>
2023-03-06 12:25:32 +0100

    tests added

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -3,6 +3,8 @@ package hu.obuda.devops.fibonaccirestapi.service
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FibonacciServiceTest {
@@ -15,7 +17,28 @@ class FibonacciServiceTest {
         // when
         val result: Int = underTest.fibonacci(1)
         // then
-        Assertions.assertEquals(0, result)
+        Assertions.assertEquals(1, result)
+    }
+
+    @Test
+    fun test46EdgeCase(){
+        //given
+
+        //when
+        val result: Int = underTest.fibonacci(46)
+
+        //then
+        Assertions.assertEquals(1836311903, result)
+    }
+
+    @Test
+    fun shouldReturn55WhenCall10() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(10)
+        // then
+        Assertions.assertEquals(55, result)
     }
 
     // TODO - Test with greater numbers and test edge cases
