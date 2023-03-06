819d799221b778ca6b7b113d21d9355c796073e3
Sly <sziszko0129@gmail.com>
2023-03-06 12:30:53 +0100

    task done

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -3,6 +3,7 @@ package hu.obuda.devops.fibonaccirestapi.service
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.springframework.web.client.RestClientException
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FibonacciServiceTest {
@@ -15,8 +16,66 @@ class FibonacciServiceTest {
         // when
         val result: Int = underTest.fibonacci(1)
         // then
-        Assertions.assertEquals(0, result)
+        Assertions.assertEquals(1, result)
     }
 
-    // TODO - Test with greater numbers and test edge cases
+    @Test
+    fun shouldReturn5WhenCall5() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(5)
+        // then
+        Assertions.assertEquals(5, result)
+    }
+
+    @Test
+    fun shouldReturn55WhenCall10() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(10)
+        // then
+        Assertions.assertEquals(55, result)
+    }
+
+    @Test
+    fun shouldReturn1836311903WhenCall46() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(46)
+        // then
+        Assertions.assertEquals(1836311903, result)
+    }
+
+    @Test
+    fun shouldThrowWhenCall47() {
+        // given
+
+        // when
+        val thrown = Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            var result = underTest.fibonacci(47)
+        }
+
+        // then
+        Assertions.assertNotNull(thrown)
+    }
+
+    @Test
+    fun shouldThrownWhenCallNegative() {
+        // given
+
+        // when
+        val thrown = Assertions.assertThrows(IndexOutOfBoundsException::class.java) {
+            var result = underTest.fibonacci(-1)
+        }
+
+        // then
+        Assertions.assertNotNull(thrown)
+    }
+
+
+
+
 }
