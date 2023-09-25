e71762685f3fda39ec27b3564d39a3cd00100ce6
npatrik <nagy.patrik024@gmail.com>
2023-09-25 12:04:51 +0200

    [FEATURE] added fibonacci logic

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
@@ -2,6 +2,8 @@ package hu.obuda.devops.fibonaccirestapi.controller
 
 import hu.obuda.devops.fibonaccirestapi.service.FibonacciService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -15,8 +17,12 @@ class FibonacciController {
     var fibonacciService: FibonacciService? = null
 
     @GetMapping(value = ["fibonacci"])
-    open fun fibonacci(@RequestParam n: Int): Int? {
+    open fun fibonacci(@RequestParam n: Int): ResponseEntity<Int>? {
+        if (n > 46)
+        {
+            return ResponseEntity(HttpStatus.BAD_REQUEST)
+        }
         // TODO - If n is greater than 46 then return BAD REQUEST use HttpStatus
-        return fibonacciService?.fibonacci(n)
+        return ResponseEntity<Int>(fibonacciService?.fibonacci(n), HttpStatus.OK)
     }
 }
