957d185854733506bfc9b15aac07f8ae300871d7
NagyMatyas <nagymatyas87@gmail.com>
2023-09-25 12:31:58 +0200

    Multiple test cases have been merged into parametrized test.

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -3,28 +3,27 @@ package hu.obuda.devops.fibonaccirestapi.service
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FibonacciServiceTest {
 
     private val underTest = FibonacciService()
-    @Test
-    fun shouldReturn0WhenCall1() {
+    @ParameterizedTest
+    @CsvSource("1, 1",
+               "2, 1",
+               "3, 2",
+               "4, 3",
+               "5, 5",
+               "6, 8",
+               "46, 1836311903")
+    fun shouldReturn0WhenCall1(n: Int, expectedResult: Int ) {
         // given
 
         // when
-        val result: Int = underTest.fibonacci(1)
+        val result: Int = underTest.fibonacci(n)
         // then
-        Assertions.assertEquals(1, result)
-    }
-
-    @Test
-    fun shouldReturn1836311903WhenCall46() {
-        // given
-
-        // when
-        val result: Int = underTest.fibonacci(46)
-        // then
-        Assertions.assertEquals(1836311903, result)
+        Assertions.assertEquals(expectedResult, result)
     }
 }
