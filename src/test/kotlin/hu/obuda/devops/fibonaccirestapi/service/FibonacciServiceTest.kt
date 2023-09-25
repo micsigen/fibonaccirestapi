4e83dbfd40e0520369f77760e1d62977eb401a5e
NagyMatyas <nagymatyas87@gmail.com>
2023-09-25 12:19:59 +0200

    Fibonacci calculation has been implemented

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -15,8 +15,16 @@ class FibonacciServiceTest {
         // when
         val result: Int = underTest.fibonacci(1)
         // then
-        Assertions.assertEquals(0, result)
+        Assertions.assertEquals(1, result)
     }
 
-    // TODO - Test with greater numbers and test edge cases
+    @Test
+    fun shouldReturn1836311903WhenCall46() {
+        // given
+
+        // when
+        val result: Int = underTest.fibonacci(46)
+        // then
+        Assertions.assertEquals(1836311903, result)
+    }
 }
