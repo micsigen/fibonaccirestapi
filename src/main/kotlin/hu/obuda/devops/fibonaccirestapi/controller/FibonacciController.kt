8855e0b4339026faa555b50d50cb6dd0e9352bf1
Istyle55 <balatoniisti@gmail.com>
2024-02-19 12:22:26 +0100

    feat: implementation

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
@@ -2,10 +2,12 @@ package hu.obuda.devops.fibonaccirestapi.controller
 
 import hu.obuda.devops.fibonaccirestapi.service.FibonacciService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/")
@@ -17,6 +19,7 @@ class FibonacciController {
     @GetMapping(value = ["fibonacci"])
     open fun fibonacci(@RequestParam n: Int): Int? {
         // TODO - If n is greater than 46 then return BAD REQUEST use HttpStatus
+        if (n >= 47) throw ResponseStatusException(HttpStatus.BAD_REQUEST)
         return fibonacciService?.fibonacci(n)
     }
 }
--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,8 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 0
-        else 0 // TODO instead of this logic implement fibonacci
+        if (n == 0) return 0
+        return if (n == 1) 1
+        else return fibonacci(n-1) + fibonacci(n-2) // TODO instead of this logic implement fibonacci
     }
 }
