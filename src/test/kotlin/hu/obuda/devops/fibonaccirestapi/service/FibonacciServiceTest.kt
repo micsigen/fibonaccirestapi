08a55f5597d11dbd888b7643701d0cf337363210
bobiuszgithub <bcsaldi18@gmail.com>
2024-02-19 12:27:57 +0100

    az komcsi

--- src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
+++ src/test/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciServiceTest.kt
@@ -9,14 +9,25 @@ class FibonacciServiceTest {
 
     private val underTest = FibonacciService()
     @Test
-    fun shouldReturn0WhenCall1() {
+    fun shouldReturn1WhenCall1() {
         // given
 
         // when
         val result: Int = underTest.fibonacci(1)
         // then
-        Assertions.assertEquals(0, result)
+        Assertions.assertEquals(1, result)
     }
 
+
+
     // TODO - Test with greater numbers and test edge cases
+    @Test
+    fun shouldReturn2WhenCall3(){
+
+        val result: Int = underTest.fibonacci(3)
+
+        Assertions.assertEquals(2, result)
+
+    }
+
 }
