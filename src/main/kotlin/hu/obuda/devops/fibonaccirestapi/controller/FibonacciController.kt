41e7b21e907142a17cbf0fd855c705fee90585a2
szigetkozitibor <szigetkozitibor@gmail.com>
2025-09-08 12:13:32 +0200

    Implementations

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/controller/FibonacciController.kt
@@ -2,9 +2,12 @@ package hu.obuda.devops.fibonaccirestapi.controller
 
 import hu.obuda.devops.fibonaccirestapi.service.FibonacciService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,8 +18,13 @@ class FibonacciController {
     var fibonacciService: FibonacciService? = null
 
     @GetMapping(value = ["fibonacci"])
-    open fun fibonacci(@RequestParam n: Int): Int? {
+    open fun fibonacci(@RequestParam n: Int): ResponseEntity<Int> {
+
         // TODO - If n is greater than 46 then return BAD REQUEST use HttpStatus
-        return fibonacciService?.fibonacci(n)
+        if (n > 46 || n < 1)
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        else return ResponseEntity.ok(fibonacciService?.fibonacci(n))
     }
 }
